2de25d843da80c27dea85dac9cb332b3ec8cef08
Alisson Teles Fraga <alissontfraga@gmail.com>
2025-04-01 00:35:51 -0300

    Initial commit

---
+++ MyFirstProject/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main (String[] args){
+        System.out.println("hello alisso"); //também funciona usar \n
+        System.out.println("ola aly");
+        System.out.println();  // digite sout para autocompletar <
+
+
+    }
+}
