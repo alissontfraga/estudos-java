b1af55779ddc2375c95b1f43879477df922a50fc
Alisson Teles Fraga <alissontfraga@gmail.com>
2025-04-03 01:15:43 -0300

    new exercises

---
+++ arithmetic/src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args){
+
+    //int x = 10;
+    //int y = 3;
+    //int z;
+
+    //z = x + y;
+    // z = x - y;
+    //z = x * y;
+    //z = x / y;
+    //z = x % y;
+    //System.out.println(z);
+
+
+    //Augmented Assignment Operators
+
+    //int x = 10;
+    //int y = 3;
+
+        //x += y;    <   //x = x + y;
+        //x -= y;    <   //x = x - y;
+        //x *= y;    <  // x = x * y;
+        //x /= y;    <  //x = x / y;  se atentar que se dividir um inteiro apenas será inteiro
+        //x  %= y;   <  //x = x % y;
+
+        //Increment and decrement operators
+
+        int x = 1;
+        //x++;  // to increase and decrement one
+        //x--;
+      //System.out.println(x);
+
+
+        //Order of operations  [P-E-M-D-A-S]
+    // Parentêses-Exponenciação-Multiplicação-Divisão-Adição-Subtração
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+        System.out.println(result);
+    }
+}
