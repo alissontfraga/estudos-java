90e5dcb0b3f77ec9b1547e06160a7878dc57e656
Alisson Teles Fraga <alissontfraga@gmail.com>
2025-04-03 22:24:12 -0300

    new exercises

---
+++ ifStatements/src/Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+    Scanner scanner = new Scanner(System.in);
+
+    String name;
+    int age;
+    boolean isStudent;
+
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
+
+        System.out.print("Are you a student? (true/false): ");
+        isStudent = scanner.nextBoolean();
+
+
+        //name
+
+        if(name.isEmpty()){
+            System.out.println("You didn't enter your name!");
+        }
+        else{
+            System.out.println("Hello " + name + "!");
+        }
+
+
+        //age
+    if(age >= 65){
+            System.out.println("You are a senior!");
+    }
+
+    else if(age >= 18){
+        System.out.println("You are an adult!");
+    }
+
+    else if(age < 0){
+        System.out.println("You haven't been born yet!");
+    }
+
+    else if(age == 0){
+        System.out.println("You are a baby!");
+    }
+
+    else{
+        System.out.println("You are a child!");
+    }
+
+        //isStudent
+
+        if(isStudent){
+            System.out.println("You are a student!");
+        }
+        else{
+            System.out.println("You are not a student!");
+        }
+
+        scanner.close();
+
+    }
+
+
+}
