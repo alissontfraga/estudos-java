1883deaba668dcf9a2b5d1623c7c7321a6f1b46b
Alisson Teles Fraga <alissontfraga@gmail.com>
2025-05-07 00:27:33 -0300

    New exercises

--- beecrowd/src/Main.java
+++ beecrowd/src/Main.java
@@ -5,26 +5,25 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-       int codigo1;
-       int quantidade1;
-       double preco1;
-       int codigo2;
-       int quantidade2;
-       double preco2;
-       double total;
+        double A;
+        double B;
+        double C;
 
+        A = scanner.nextDouble();
+        B = scanner.nextDouble();
+        C = scanner.nextDouble();
 
-       codigo1 = scanner.nextInt();
-       quantidade1 = scanner.nextInt();
-       preco1 = scanner.nextDouble();
+        double areaTriangulo = (A * C) / 2;
+        double areaCirculo = 3.14159 * (Math.pow(C, 2));
+        double areaTrapezio = (A + B) * C / 2;
+        double areaQuadrado = (Math.pow(B,2));
+        double areaRetangulo = A * B;
 
-       codigo2 = scanner.nextInt();
-       quantidade2 = scanner.nextInt();
-       preco2 = scanner.nextDouble();
-
-       total = (quantidade1 * preco1) + (quantidade2 * preco2);
-
-        System.out.printf("VALOR A PAGAR: R$ %.2f\n", total);
+        System.out.printf("TRIANGULO: %.3f\n", areaTriangulo);
+        System.out.printf("CIRCULO: %.3f\n", areaCirculo);
+        System.out.printf("TRAPEZIO: %.3f\n", areaTrapezio);
+        System.out.printf("QUADRADO: %.3f\n", areaQuadrado);
+        System.out.printf("RETANGULO: %.3f\n", areaRetangulo);
 
         scanner.close();
     }
